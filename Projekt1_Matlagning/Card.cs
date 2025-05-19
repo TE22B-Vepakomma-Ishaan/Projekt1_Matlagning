3e039516f0e230e920753e18fe2060082c8e396e
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2025-05-19 10:13:36 +0200

    1

---
+++ Projekt1_Matlagning/Card.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Projekt1_Matlagning;
+
+public class Card
+{
+    public string Suit { get; set; }
+
+    public int Num { get; set; }
+
+    public void CardRender()
+    {
+
+        string x = Num switch
+        {
+            1 => "A",
+            11 => "J",
+            12 => "Q",
+            13 => "K",
+            _ => Num.ToString()
+        };
+
+        string z = Suit switch
+        {
+            "Spade" => "♠",
+            "Club" => "♣",
+            "Diamond" => "♦",
+            "Heart" => "♥",
+            "Special" => "🏶",
+        };
+     
+    }
+}
